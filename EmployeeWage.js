a112b880848f2f4f63d4746550c226bdee9b5cb9
Kanishk066 <kanishksinghal06@gmail.com>
2023-06-02 19:20:25 +0530

    Added the code for calculating monthly wage for multiple companies

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,34 +1,25 @@
 class EmployeeWageCalculator {
-    static NUM_WORKING_DAYS = 20;
-    static IS_PART_TIME = 1;
-    static IS_FULL_TIME = 2;
-    static PART_TIME_HOUR = 4;
-    static WAGE_PER_HOUR = 20;
-    static FULL_DAY_HOUR = 8;
-    static MAX_WORKING_HOURS = 100;
-  
     constructor() {
+      this.PART_TIME_HOUR = 4;
+      this.FULL_DAY_HOUR = 8;
       this.totalWorkingDays = 0;
       this.totalWorkingHours = 0;
       this.monthlyWage = 0;
     }
   
-    calculateWage() {
-      console.log("Welcome to Employee Wage Project");
+    calculateWage(company, wagePerHour, numWorkingDays, maxWorkingHours) {
+      console.log("Calculating wage for company " + company);
       let dailyWage;
-      while (
-        this.totalWorkingDays < EmployeeWageCalculator.NUM_WORKING_DAYS &&
-        this.totalWorkingHours < EmployeeWageCalculator.MAX_WORKING_HOURS
-      ) {
+      while (this.totalWorkingDays < numWorkingDays && this.totalWorkingHours < maxWorkingHours) {
         const newAttendance = Math.floor(Math.random() * 3); // 0 for absent, 1 for part-time, 2 for full-time
         switch (newAttendance) {
-          case EmployeeWageCalculator.IS_PART_TIME:
-            dailyWage = EmployeeWageCalculator.WAGE_PER_HOUR * EmployeeWageCalculator.PART_TIME_HOUR;
-            this.totalWorkingHours += EmployeeWageCalculator.PART_TIME_HOUR;
+          case 1:
+            dailyWage = wagePerHour * this.PART_TIME_HOUR;
+            this.totalWorkingHours += this.PART_TIME_HOUR;
             break;
-          case EmployeeWageCalculator.IS_FULL_TIME:
-            dailyWage = EmployeeWageCalculator.WAGE_PER_HOUR * EmployeeWageCalculator.FULL_DAY_HOUR;
-            this.totalWorkingHours += EmployeeWageCalculator.FULL_DAY_HOUR;
+          case 2:
+            dailyWage = wagePerHour * this.FULL_DAY_HOUR;
+            this.totalWorkingHours += this.FULL_DAY_HOUR;
             break;
           default:
             dailyWage = 0;
@@ -37,18 +28,18 @@ class EmployeeWageCalculator {
         this.totalWorkingDays++;
         this.monthlyWage += dailyWage;
       }
-    }
-  
-    printWage() {
-      console.log("Total Working Days = " + this.totalWorkingDays);
-      console.log("Total Working Hours = " + this.totalWorkingHours);
-      console.log("Employee Monthly Wage = " + this.monthlyWage);
+      return this.monthlyWage;
     }
   }
   
-  const empWage = new EmployeeWageCalculator();
-  empWage.calculateWage();
-  empWage.printWage();
+  const companyA = new EmployeeWageCalculator();
+  const wageA = companyA.calculateWage("Company A", 20, 20, 100);
+  console.log("Monthly wage for Company A: " + wageA);
+  
+  const companyB = new EmployeeWageCalculator();
+  const wageB = companyB.calculateWage("Company B", 25, 25, 120);
+  console.log("Monthly wage for Company B: " + wageB);
+  
   
 
 
