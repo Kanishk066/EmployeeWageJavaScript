d680cedde61aa6409dcb03cbddfbfba8fb31d2ef
Kanishk066 <kanishksinghal06@gmail.com>
2023-06-02 18:35:40 +0530

    Added the code to  calculate the wage of part time employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,15 +1,25 @@
-console.log("Welcome to the employee wage problem");
+console.log("Welcome to Employee Wage Project");
+
 const IS_PRESENT = 1;
 const WAGE_PER_HOUR = 20;
 const FULL_DAY_HOUR = 8;
 let dailyWage = 0;
 
-// Generate a random number between 0 and 1 (inclusive)
 let attendance = Math.floor(Math.random() * 2);
-
 if (attendance === IS_PRESENT) {
+  console.log("Employee is Present");
+} else {
+  console.log("Employee is Absent");
+}
+
+attendance = Math.floor(Math.random() * 3);
+if (attendance === 1) {
+  const PART_TIME_HOUR = 4;
+  dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+  console.log("Employee is Part Time. Daily Wage = " + dailyWage);
+} else if (attendance === 2) {
   dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-  console.log("Employee is Present. Daily Wage = " + dailyWage);
+  console.log("Employee is Full Time. Daily Wage = " + dailyWage);
 } else {
   console.log("Employee is Absent. Daily Wage = " + dailyWage);
 }
