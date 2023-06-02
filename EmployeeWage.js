9ea5613ba5285a51f5fbb8c5173c9c0d45032795
Kanishk066 <kanishksinghal06@gmail.com>
2023-06-02 18:23:22 +0530

    Added the code to calculate the daily Employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,15 @@
 console.log("Welcome to the employee wage problem");
+const IS_PRESENT = 1;
+const WAGE_PER_HOUR = 20;
+const FULL_DAY_HOUR = 8;
+let dailyWage = 0;
+
+// Generate a random number between 0 and 1 (inclusive)
+let attendance = Math.floor(Math.random() * 2);
+
+if (attendance === IS_PRESENT) {
+  dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+  console.log("Employee is Present. Daily Wage = " + dailyWage);
+} else {
+  console.log("Employee is Absent. Daily Wage = " + dailyWage);
+}
